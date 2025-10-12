0bb8dfa8f7296b7c54387f19103bef08bef43652
username54321-dotcom <marwankam3l@gmail.com>
2025-10-13 00:54:04 +0300

    ****possible fix

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,8 +1,11 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  process.env.EXPO_SUPABASE_BASE_URL || 'https://hdxnyotrpjmrigmpdpkn.supabase.co';
+const supabaseAnonKey =
+  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ||
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkeG55b3RycGptcmlnbXBkcGtuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk4NTQyMDgsImV4cCI6MjA3NTQzMDIwOH0.CzVGOVOXPqI4bZtlcDmmqgx0UKQRKpuR5mooSs2fLcw';
 
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey, {
   // auth: {
