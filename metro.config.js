d22c7c2211ecec39b2634c4561e8c98cca5be7f7
username54321-dotcom <marwankam3l@gmail.com>
2025-11-02 02:37:29 +0200

    working rich text on web

--- metro.config.js
+++ metro.config.js
@@ -7,4 +7,24 @@ const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
 
+/*custom*/ const webAliases = {
+  'react-native': 'react-native-web',
+  'react-native-webview': '@10play/react-native-web-webview',
+  'react-native/Libraries/Utilities/codegenNativeComponent':
+    '@10play/react-native-web-webview/shim',
+  crypto: 'expo-crypto',
+};
+config.resolver.resolveRequest = (context, realModuleName, platform, moduleName) => {
+  if (platform === 'web') {
+    const alias = webAliases[realModuleName];
+    if (alias) {
+      return {
+        filePath: require.resolve(alias),
+        type: 'sourceFile',
+      };
+    }
+  }
+  return context.resolveRequest(context, realModuleName, platform, moduleName);
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
