a379aacfb31a9d8e6ed2076ea994de65c3b53140
username54321-dotcom <marwankam3l@gmail.com>
2025-11-03 01:01:29 +0200

    rte ai proto

--- app/(drawer)/(Pages)/Books.tsx
+++ app/(drawer)/(Pages)/Books.tsx
@@ -1,7 +1,13 @@
 import Background from '~/components/Background';
+import Editor from './../../../components/Reusebales/Lexical';
+import Lexical from './../../../components/Reusebales/Lexical';
 
 const Books = () => {
-  return <Background></Background>;
+  return (
+    <Background>
+      <Lexical></Lexical>{' '}
+    </Background>
+  );
 };
 
 export default Books;
