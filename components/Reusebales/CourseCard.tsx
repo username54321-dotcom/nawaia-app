c061c0590f2d82a9ddcc0c17000ef44c28f1f176
username54321-dotcom <marwankam3l@gmail.com>
2025-11-05 23:40:04 +0200

    minor optimizations

--- components/Reusebales/CourseCard.tsx
+++ components/Reusebales/CourseCard.tsx
@@ -1,5 +1,5 @@
 import { View, Text, Pressable } from 'react-native';
-import React from 'react';
+import React, { memo } from 'react';
 import { Tables } from '~/utils/database.types';
 import FadeIn from '../Animations/FadeIn';
 import MyImage from './MyImage';
@@ -37,4 +37,4 @@ const CourseCard = ({ courseItem }: { courseItem: Tables<'courses'> }) => {
   );
 };
 
-export default CourseCard;
+export default memo(CourseCard);
