ff07a6f8395dfb0b196a3964afc7b3561b580ed0
username54321-dotcom <marwankam3l@gmail.com>
2025-11-07 11:37:26 +0200

    minor

--- components/Pages/[id]/TelegramButton.tsx
+++ components/Pages/[id]/TelegramButton.tsx
@@ -1,6 +1,6 @@
 import { Text, Pressable, Linking, View } from 'react-native';
-import MyImage from '~/components/Reusebales/MyImage';
 import { useIsAuth, useModalVisible } from '~/store/store';
+import MyImage1 from '~/components/Reusebales/MyImage';
 
 const TelegramButton = ({ telegramLink }: { telegramLink: string | null | undefined }) => {
   const logo = require('assets/svg/telegram_.png');
@@ -15,9 +15,9 @@ const TelegramButton = ({ telegramLink }: { telegramLink: string | null | undefi
       onPress={handleOnPress}
       className=" size-fit flex-row-reverse items-center justify-center rounded-xl border-[1px] border-slate-400 bg-sky-500 px-4 py-2 transition-all duration-200 hover:scale-105 hover:bg-sky-200">
       <View className="rounded-full bg-white p-[2px]">
-        <MyImage
+        <MyImage1
           source={logo}
-          className="bg-MyImage size-[20px] translate-x-[-0.5]  rounded-full "></MyImage>
+          className=" size-[20px] translate-x-[-0.5]  rounded-full "></MyImage1>
       </View>
       {/* <Text className=" mr-1  font-Kufi text-xs font-semibold text-neutral-600">
         جروب التليجرام
