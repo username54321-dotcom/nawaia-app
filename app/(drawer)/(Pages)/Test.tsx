011b614db4fb1c207639e28eb5df8b4b578db27c
username54321-dotcom <marwankam3l@gmail.com>
2025-11-12 11:01:45 +0200

    test

--- app/(drawer)/(Pages)/Test.tsx
+++ app/(drawer)/(Pages)/Test.tsx
@@ -3,22 +3,14 @@ import { useEffect } from 'react';
 import Background from '~/components/Background';
 import { MotiView, useAnimationState } from 'moti';
 import { style } from 'twrnc';
-
+import { LottieView } from './../../../node_modules/lottie-react-native/src/LottieView/index';
 const Test = ({ show }: { show: boolean }) => {
-  const animation = useAnimationState({
-    hide: { scaleX: 0 },
-    show: { scaleX: 1 },
-  });
-  useEffect(() => {
-    show && animation.transitionTo('show');
-    !show && animation.transitionTo('hide');
-  });
   return (
-    <Background>
-      <MotiView style={style('absolute right-0', { transformOrigin: 'right' })} state={animation}>
-        <View className="h-96 w-[30vw]  bg-blue-400"></View>
-      </MotiView>
-    </Background>
+    <>
+      <Background>
+        <LottieView></LottieView>
+      </Background>
+    </>
   );
 };
 
