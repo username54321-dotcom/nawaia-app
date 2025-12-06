d7ea2e2c76563f135ccb19b6aed345d63e68b7ae
username54321-dotcom <marwankam3l@gmail.com>
2025-12-06 05:01:15 +0200

    refactor(accordion): improve readability
    - Renamed LongDescription to longDescription
    - Added propTypes interface
    - Added comments for clarity
    - Reworked the isAdmin Helper Function to be an Edge Function for Security Reasons
    - Fixed Auth logic ( 1 render intead of preveously 2 )

--- components/Pages/[id]/TextAccordion.tsx
+++ components/Pages/[id]/TextAccordion.tsx
@@ -1,16 +1,15 @@
 import { View, Text, Pressable } from 'react-native';
-import { memo, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { MotiView, useAnimationState } from 'moti';
 import tw from 'twrnc';
 import { LinearGradient } from 'expo-linear-gradient';
 
-const TextAccordion = ({
-  shortDescription,
-  LongDescription,
-}: {
+interface propTypes {
   shortDescription: string;
-  LongDescription: string;
-}) => {
+  longDescription: string;
+}
+
+const TextAccordion = ({ shortDescription, longDescription }: propTypes) => {
   const [TextExpand, setTextExpand] = useState(false);
   const [TextHeight, setTextHeight] = useState(300);
   const animation = useAnimationState({ from: { height: 300 }, to: { height: TextHeight } });
@@ -18,23 +17,28 @@ const TextAccordion = ({
     () => (TextExpand ? animation.transitionTo('to') : animation.transitionTo('from')),
     [TextExpand, animation]
   );
+  console.log(longDescription);
   return (
     <>
+      {/** Parent View */}
       <MotiView
         state={animation}
-        style={tw`h-[300px] overflow-hidden w-4/5 max-w-[600px] mx-4 my-6 flex size-fit rounded-md border-2 border-slate-600 bg-slate-200 pt-1 `}>
-        <View onLayout={(e) => setTextHeight(e.nativeEvent.layout.height + 50)}>
+        style={tw`h-[300px]  overflow-hidden w-4/5 max-w-[600px] mx-4 my-6 flex size-fit rounded-md border-2 border-slate-600 bg-slate-200 pt-1 `}>
+        {/** Text View */}
+        <View className=" " onLayout={(e) => setTextHeight(e.nativeEvent.layout.height + 50)}>
           <Text className=" m-2  text-center font-Kufi   text-lg font-semibold  text-neutral-700">
             {shortDescription}
           </Text>
+          {/** Separator */}
           <View className="m-4 h-1 w-4/5 self-center border-t-2 opacity-75"></View>
           <Text
-            className={`z-10 m-2 mb-0 w-[90%] self-center  px-4 text-right  font-Kufi text-base text-neutral-700 transition-all  duration-300 `}>
-            {LongDescription}
+            className={` m-2 mx-auto  mb-0  w-[90%] border-4  px-4 text-right font-Kufi text-base  text-neutral-700 `}>
+            {longDescription}
           </Text>
-
+          {/** Separator */}
           <View className="pb-22 my-4 h-1 w-4/5 self-center border-t-2 opacity-75"></View>
         </View>
+        {/** Gradient View */}
         <View className="absolute bottom-0 h-12 w-full  ">
           <LinearGradient
             locations={[0.1, 1]}
@@ -43,7 +47,7 @@ const TextAccordion = ({
           <Pressable
             className="  absolute bottom-0 w-full flex-col items-center justify-end "
             onPress={() => setTextExpand((state) => !state)}>
-            <View className="mb-2 rounded-lg border-[1px] bg-red-700/90">
+            <View className="mb-2  rounded-lg border-[1px] bg-red-700/90">
               <Text className="m-2 font-Kufi font-semibold leading-3     text-slate-100 ">
                 {TextExpand ? 'أخفاء' : 'عرض المزيد'}
               </Text>
@@ -55,4 +59,4 @@ const TextAccordion = ({
   );
 };
 
-export default memo(TextAccordion);
+export default TextAccordion;
